7ae1d783504e2d3e86f09a1bfc4e97b714b83ab3
Sudeep Mahato <sudeepmahato@Sudeeps-MacBook-Air.local>
2023-12-20 18:39:02 +0530

    code fix

--- anagram.js
+++ anagram.js
@@ -7,3 +7,7 @@ function areAnagrams(str1, str2) {
 }
 
 console.log(areAnagrams("abba","baba"));
+
+// Token - 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
+//git remote set-url origin https://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@github.com/sudeepmahato9/CodingPractice
+// git remote set-url origin git@github.com:sudeepmahato9/CodingPractice.git
